4baaaecc4da25c98fb67940078f0a140c41ae7cb
hakuna matata <93985109+justsetia@users.noreply.github.com>
2022-02-05 11:37:38 -0800

    Delete python/h2_az_computer directory

--- python/h2_az_computer/is_prime.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-for i in range(2,num,):
-    if num%i==0:
-        print('not prime')
-        break
-    elif i==num-1:
-        print('prime')
-    
-    
--- python/h2_az_computer/odd or even.py
+++
@@ -1,5 +0,0 @@
-num = int(input())
-if num%2!=0:
-    print('odd')
-else:
-    print('even')
